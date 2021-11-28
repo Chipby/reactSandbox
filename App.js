6120634556ab9df9dfe25017d13ae10271445ac0
tony <owner@owners-mbp.attlocal.net>
2021-11-28 12:07:42 -0800

    added inputs and a little styling

--- App.js
+++ App.js
@@ -1,12 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, View } from "react-native";
+import { Text,TextInput, Title } from '@shoutem/ui';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey boss am good with the app Setup now !! let move</Text>
-      <Text>Awesome! - Tony</Text>
+      <Text>Welcome to the app...</Text>
+      <TextInput
+      style={styles.input}
+  defaultValue={'Username or email'}
+  onChangeText={()=>{}}
+/>
+<TextInput
+style={styles.input}
+  placeholder={'Password'}
+  secureTextEntry
+/>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,8 +25,16 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
+    backgroundColor: "#F8F8F8",
     alignItems: "center",
     justifyContent: "center",
+  },
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+    borderColor: 'red',
+    width: 350,
   },
 });
